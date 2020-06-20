c166e3b634eb266b4a193ef0bb18729c5f7a980a
sourab <Sourabkapoor@yahoo,com>
2020-06-20 17:17:33 +0530

    Updates

---
+++ precache-manifest.aafe7c6f9e8517a0125cdc2b4c1418ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853618d8fb1361d876f5b0249bdbe9fb",
+    "url": "/myCart/index.html"
+  },
+  {
+    "revision": "4ad3b1bb9ecdd141137b",
+    "url": "/myCart/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0deef3c9732ed04e1813",
+    "url": "/myCart/static/css/main.7f7719df.chunk.css"
+  },
+  {
+    "revision": "4ad3b1bb9ecdd141137b",
+    "url": "/myCart/static/js/2.205a6f98.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/myCart/static/js/2.205a6f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0deef3c9732ed04e1813",
+    "url": "/myCart/static/js/main.be6ad608.chunk.js"
+  },
+  {
+    "revision": "4eced221584a6111da90",
+    "url": "/myCart/static/js/runtime-main.b24076ac.js"
+  }
+]);
