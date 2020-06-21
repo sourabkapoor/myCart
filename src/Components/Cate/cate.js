07371813d881024e2effed1dd03560f3ce3e0075
sourab <Sourabkapoor@yahoo,com>
2020-06-21 15:54:48 +0530

    new changes

--- src/Components/Cate/cate.js
+++ src/Components/Cate/cate.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Cate = ({names, index}) => {
 	const category = names.map((hits, i) => {
 		return (
-			<div className='w-20 pa4 mr2 mb2 br3 dib br3 pd3 ma2 mr2 grow bw2 shadow-4 bg-washed-yellow pointer'
+			<div className='w-20 pa4 mr2 mb2 br3 dib ma3 grow shadow-4 bg-washed-yellow pointer h4'
 			key={i} onClick={()=> index(i)} >
-				{hits.className}
+				<div className='mt3'>{hits.className}</div>
 			</div>
 		)
 	});
@@ -18,10 +18,3 @@ const Cate = ({names, index}) => {
 }
 
 export default Cate;
-
-
-
-
-
-
-
